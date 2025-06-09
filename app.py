f1f7ff3f2f50392ca36c40f0044fb86ea7931190
Omar Fathy <168038998+omarfathyz8@users.noreply.github.com>
2025-06-09 04:03:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ if st.button("🔍 Analyze Prompt"):
         st.markdown("---")
         st.subheader("🔒 Final Decision")
         
-        if result_map[combined] == "Injected":
+        if result_map[combined] == 1:
             st.error("🚨 Prompt is potentially **Injected**")
         else:
             st.success("✅ Prompt is **Safe**")
